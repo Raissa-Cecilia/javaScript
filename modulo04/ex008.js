ef91f216bdf41950b3321e1e39480ddc9b26a282
Raissa-Cecilia <raissabussines05@gmail.com>
2025-08-14 13:51:46 -0300

    js condição ex008

---
+++ modulo04/ex008.js
@@ -0,0 +1,6 @@
+var vel=55.5
+console.log(`A velocidade do seu carro é ${vel}`)
+if(vel>60){
+    console.log("você passou o limite de velocidade")
+}
+console.log("Dirija sempre usando cinto de segurança")
