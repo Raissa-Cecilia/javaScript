0cc68ed1b27a76eefe1eadb8ff090ffe6541b89f
Raissa-Cecilia <raissabussines05@gmail.com>
2025-08-28 15:37:42 -0300

    finalizando verificador de idade

--- modulo04/exercicoIdades/script.js
+++ modulo04/exercicoIdades/script.js
@@ -12,33 +12,39 @@ function verificar(){
         var gênero = ''
         var img= document.createElement('img')
         img.setAttribute('id','foto')
+        img.style.display = 'block'
+        img.style.margin = 'auto'
+        img.style.marginTop = '10px'
         
         if(fsex[0].checked){
             gênero = 'Homem'
             if(idade >=0 && idade <10){
-                
+                img.setAttribute('src','Bhomem.jpg')
             }else if(idade <21){
-        
+                img.setAttribute('src','Yhomem.jpg')
             } else if(idade <50){
-        
+                img.setAttribute('src','Homem.jpg')
             } else {
-        
+                img.setAttribute('src','Ohomem.jpg')
             }
         }
         else if (fsex[1].checked){
             gênero = 'Mulher'
               if(idade >=0 && idade <10){
-                
+                img.setAttribute('src','Bmulher.jpg')
             }else if(idade <21){
-        
+                img.setAttribute('src','Ymulher.jpg')
             } else if(idade <50){
-        
+                img.setAttribute('src','mulher.jpg')
             } else {
-        
+                img.setAttribute('src','Omulher.jpg')
             }
         }
 
         res.style.textAlign='center'
         res.innerHTML=`Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
+        
+
     }
 }
