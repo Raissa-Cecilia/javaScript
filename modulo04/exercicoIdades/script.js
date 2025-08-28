caa423c0fa22a5571eacc44b38efed569912c204
Raissa-Cecilia <raissabussines05@gmail.com>
2025-08-28 15:23:30 -0300

    Exercicio de verificador de idade

---
+++ modulo04/exercicoIdades/script.js
@@ -0,0 +1,44 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano= document.getElementById('txtano')
+    var res = document.getElementById('res')
+
+    if(fano.value.length == 0 || fano.value > ano){
+        window.alert('Verifique os dados e tente novamente!')
+    }else {
+        var fsex=document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img= document.createElement('img')
+        img.setAttribute('id','foto')
+        
+        if(fsex[0].checked){
+            gênero = 'Homem'
+            if(idade >=0 && idade <10){
+                
+            }else if(idade <21){
+        
+            } else if(idade <50){
+        
+            } else {
+        
+            }
+        }
+        else if (fsex[1].checked){
+            gênero = 'Mulher'
+              if(idade >=0 && idade <10){
+                
+            }else if(idade <21){
+        
+            } else if(idade <50){
+        
+            } else {
+        
+            }
+        }
+
+        res.style.textAlign='center'
+        res.innerHTML=`Detectamos ${gênero} com ${idade} anos.`
+    }
+}
