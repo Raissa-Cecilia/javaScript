c76bbe501cbca9dd14d14d375b9423481a8c3068
Raissa-Cecilia <raissabussines05@gmail.com>
2025-09-02 09:34:07 -0300

    desafio do contador

---
+++ modulo05/exDesafio01/script.js
@@ -0,0 +1,26 @@
+function contar(){
+    let ini = document.getElementById('txtI')
+    let fim = document.getElementById('txtF')
+    let passo = document.getElementById('txtP')
+    var res = document.getElementById('res')
+        if(ini.value.length ==0 || fim.value.length ==0 || passo.valuelength ==0){
+            window.alert('[ERRO] Faltam dados!');
+        }
+        else{
+            res.innerHTML='Contando....'
+            let i=Number(ini.value)
+            let f=Number(fim.value)
+            let p=Number(passo.value)
+            if(i<f){
+            for(let c=i;c<=f;c+=p){
+                res.innerHTML+=`${c} \u{1F449}`
+            }
+        }else{
+            for(let c=i;c>=f;c-=p){
+                res.innerHTML+=`${c} \u{1F449}`
+            }
+        }
+            res.innerHTML+=`\u{1F3C1}`
+        }
+    
+}
